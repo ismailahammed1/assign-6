dc5e705860a9e183129bdfbd0e4716fc16d04148
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-05-26 00:44:31 +0600

    Your commit message

--- js/index.js
+++ js/index.js
@@ -1,190 +1,85 @@
-const loadALlNews = async () => {
-    try {
-        const url = `https://openapi.programming-hero.com/api/news/categories`;
-        const res = await fetch(url);
-        const data = await res.json();
-        return data
-    } catch (error) {
-        console.error(error);
-    }
-    // console.log(data.data.news_category[2].category_name);
-    // displayNewsDataApi(data.data);
-}
-/*-------------- menu & catagory select--------------*/
-const uniqueArray = [];
-console.log(uniqueArray.length);
-const dinamicItemValue = () => {
-    const value = document.getElementById("dinamic-Velue")
-    value.innerText = `${uniqueArray.length}`
-
-}
-dinamicItemValue()
-const setAllMenu = async () => {
-    const data = await loadALlNews();
-    const AllMenu = document.getElementById('New-menu')
-
-    for (const NewsItem of data.data.news_category) {
-        /*--------------create menu--------------*/
-        if (uniqueArray.indexOf(NewsItem.category_name) === -1) {
-            uniqueArray.push(NewsItem.category_name);
-            const ul = document.createElement('ul');
-            ul.innerHTML = `<span onclick= "menubar('${NewsItem.category_id}')" class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:text-black dark-mode:focus:text-black dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-              >${NewsItem.category_name}</span>`;
-            AllMenu.appendChild(ul)
-        }
-    }
-}
-setAllMenu();
-const menubar = (category_id) => {
-    // console.log(category_id);
-    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-        .then(res => res.json())
-        .then(data => diplayNews(data.data))
-
-}
-// menubar()
-const diplayNews = allNews => {
-    console.log(allNews);
-    const newsContainer = document.getElementById('news-container')
-    newsContainer.innerHTML = '';
-    allNews.forEach(news => {
-        const newsDiv = document.createElement('div');
-        newsDiv.classList.add('w-full', 'lg:max-w-full', 'lg:flex');
-        newsDiv.innerHTML = `
-        <div
-        class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
-        <img src="${news.image_url}"/>
-    </div>
-    <div
-        class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-        <div class="mb-8">
+document.getElementById("spner").style.display = "none"
+const loadAllCategory = async()=>{
     
-            <div class="text-gray-900 break-all font-bold text-xl mb-2">${news.title.slice(0, 100)}</div>
-            <p class="text-gray-700 text-base">${news.details.slice(0, 500)}
-            </p>
-        </div>
-        <div class="flex justify-between grid grid-rows-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-col gap-4">
-            <div class="flex items-center">
-                <img class="w-10 h-10 rounded-full mr-4" src="${news.author.img}" Avatar of Writer">
-                <div class="text-sm">
-                    <p class="text-gray-900 leading-none">${news.author.name}</p>
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+    return data;
+}
+const loadAllMaun = async() =>{
+   const data = await loadAllCategory();
+   const listMaun = data.data.news_category;
 
-         
-                </div>
-            </div>
-            <div class="flex items-center ">
-            <i class="fa fa-eye mr-4 mt-1" aria-hidden="true"></i>
-                <div class="text-sm">
-                    <p class="text-gray-600">${news.total_view}</p>
-                </div>
-            </div>
-            <div class="flex items-center md:d-none">
-                <div class="flex items-center">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star-half-stroke"></i>
-                </div>
-                <div class="text-sm">
-                </div>
-            </div>
-            <div class="flex items-center">
-            <button onclick="toggleModal('modal-id')" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow cursor-pointer">
-            <i class="fa-solid fa-arrow-right"></i>
-            
-          </button>
-         
-        <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="modal-id">
-          <div class="relative w-auto my-6 mx-auto max-w-3xl">
-            <!--content-->
-            <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              <!--header-->
-              <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <h3 class="text-3xl font-semibold">
-                ${news.title}
-                </h3>
-                <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onclick="toggleModal('modal-id')">
-                  <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                    ×
-                  </span>
-                </button>
-              </div>
-              <!--body-->
-              <div class="relative p-6 flex-auto">
-                <p class="my-4 text-slate-500 text-lg leading-relaxed">
-                ${news.details}
-                </p>
-              </div>
-              <!--footer-->
-              <div class="flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
-              <p class="text-gray-900 leading-none">${news.author.name}</p>
+   const menu = document.getElementById('all-menu');
+   for(const list of listMaun){
+    
+    const li = document.createElement("li");
+    li.innerHTML = `<button onclick="catagories('${list.category_id}')">${list.category_name}</button>
+    `;
+    menu.appendChild(li);
+    
+   }
+}
+loadAllMaun();
 
-              <p class="text-gray-600">${news.author.published_date}</p>
-              <div class="flex items-center ">
-            <i class="fa fa-eye mr-4 mt-1" aria-hidden="true"></i>
-                <div class="text-sm">
-                    <p class="text-gray-600">${news.total_view}</p>
-                </div>
-            </div>
-            <div class="flex items-center md:d-none">
-                <div class="flex items-center">
+// maun part end 
+
+const catagories = async(categoryId) =>{
+    document.getElementById("spner").style.display = "block"
+const category = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
+    const data = await category.json();
+    const newsItems = data.data;
+    countString = document.getElementById('count-number').innerText = newsItems.length;
+
+    const newsList = document.getElementById('all-data');
+    newsList.innerHTML = '';
+    newsItems.forEach(singleNews => {
+        document.getElementById("spner").style.display = "none" 
+        const {title,total_view, author, details, thumbnail_url, _id} = singleNews
+        const newsCard = document.createElement('div');
+        newsCard.classList.add('col')
+        newsCard.innerHTML=`
+      <div class="card lg:card-side bg-base-100 shadow-xl">
+      <figure><img src="${thumbnail_url}" class="h-84 w-64 p-4" alt="Movie"></figure>
+        <div class="card-body">
+            <h2 class="card-title text-purple-500">${title.length > 80 ? title.slice(0,50)+ '....' : title}</h2>
+            <p>${details.length > 900 ? details.slice(0,500)+ '.....' : details}</p>
+                <div class="flex flex-row items-center">
+                <div><p><img src="${author.img}" class="h-20 w-20 p-4 rounded-full"</p></div>
+                <div class="basis-1/4 "><p>${author.name ? author.name : 'N/A'}</p></div>
+                <div class="ps-6"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" stroke="#000" stroke-width="2" d="M12,21 C7,21 1,16 1,12 C1,8 7,3 12,3 C17,3 23,8 23,12 C23,16 17,21 12,21 Z M12,7 C9.23875,7 7,9.23875 7,12 C7,14.76125 9.23875,17 12,17 C14.76125,17 17,14.76125 17,12 C17,9.23875 14.76125,7 12,7 L12,7 Z"/>
+                </svg></div>
+                <div class="basis-1/2 p-4">Total view ${total_view ? total_view : 'N/A'}</div>
+                <label onclick="newsDetail('${_id}')" for="my-modal-5" class="btn modal-button">more...</label>
                 
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star-half-stroke"></i>
                 </div>
-                <div class="text-sm">
-                </div>
-            </div>
-                <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="toggleModal('modal-id')">
-                  Close
-                </button>
-            
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-backdrop"></div>
-        <script type="text/javascript">
-         
-        </script>
-              </div>
-          </div>
+                
             </div>
         </div>
-    </div>
-    
-    `
-        newsContainer.appendChild(newsDiv);
-
+        </div>
+        `;
+        newsList.appendChild(newsCard);
     });
+    
 }
-/*-------------Spiner section-------------------*/
-// const toggleSpinner = isLoading => {
-//     const loaderSection = document.getElementById('loader');
-//     if (isLoading) {
-//         loaderSection.classList.remove('hidden')
-//     }
-//     else {
-//         loaderSection.classList.add('hidden')
-//     }
-// }
 
-
-
-/*-------------value pass */
-
-// array.sort(function (a, b) {
-//     // Turn your strings into dates, and then subtract them
-//     // to get a value that is either negative, positive, or zero.
-//     return new (b.date) - new (a.date);
-// });
-// loadALlNews()
-function toggleModal(modalID) {
-    document.getElementById(modalID).classList.toggle("hidden");
-    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
-    document.getElementById(modalID).classList.toggle("flex");
-    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+const newsDetail = (newsId) =>{
+    fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+    .then(res => res.json())
+    .then (data => modalData(data))
 }
+const modalData = (details)=>{
+    const modalBody = document.getElementById('modal-body');;
+    modalBody.innerHTML =`
+    <div>
+            <h1 class="font-bold text-lg text-purple-700 text-3xl">${details.data[0].title}</h1>     
+            <h3 class="font-bold text-lg">Writer Name: ${details.data[0].author.name ? details.data[0].author.name : 'N/A'}</h3>
+            <h3 class="font-bold text-lg">published_date: ${details.data[0].author.published_date}</h3>
+            <div><p><img src="${details.data[0].thumbnail_url}" class=""</p></div>
+            <h1 class="font-bold text-lg">News: ${details.data[0].details}</h1>
+            <h1 class="font-bold text-lg">Total view: ${details.data[0].total_view ? details.data[0].total_view : 'N/A'}</h1>
+            <label for="my-modal-5" class="btn">cancel</label>
+    </div>
+    `;
+}
+
